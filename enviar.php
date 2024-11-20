90b12684d6c459d448b6bfff313c1fddfbd74b79
gonzalo-png <gonzalo.reinoso@davinci.edu.ar>
2024-11-20 19:55:03 -0300

    Add files via upload
    tp 2

---
+++ enviar.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+
+$para = 'gonzalo.reinoso@davinci.edu.ar';
+$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
